e89d171d1b5fdfa3d312ca1b79876b9597ba15e9
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2017-01-19 14:47:55 +0300

    tf support, tested with py3

--- examples/example_gan.py
+++ examples/example_gan.py
@@ -1,21 +1,21 @@
-import matplotlib as mpl
-
-# This line allows mpl to run with no DISPLAY defined
-mpl.use('Agg')
-
 import pandas as pd
 import numpy as np
 import os
+
 from keras.layers import Dense, Reshape, Flatten, Dropout, LeakyReLU, Activation, BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.regularizers import l1, l1l2
 from keras.callbacks import TensorBoard
-from keras.datasets import mnist
 from keras_adversarial import AdversarialModel, ImageGridCallback, simple_gan, gan_targets
 from keras_adversarial import AdversarialOptimizerSimultaneous, normal_latent_sampling
 import keras.backend as K
-from mnist_utils import mnist_data
+
+from examples.mnist_utils import mnist_data
+
+import matplotlib as mpl
+# This line allows mpl to run with no DISPLAY defined
+mpl.use('Agg')
 
 
 def batch_norm(batch_norm_mode):
@@ -32,46 +32,56 @@ def dropout_layer(dropout):
         return Activation('linear')
 
 
-def model_generator(latent_dim, input_shape, hidden_dim=1024, reg=lambda: l1(1e-5), batch_norm_mode=0):
-    return Sequential([
-        Dense(hidden_dim / 4, name="generator_h1", input_dim=latent_dim, W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        Dense(hidden_dim / 2, name="generator_h2", W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        Dense(hidden_dim, name="generator_h3", W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        Dense(np.prod(input_shape), name="generator_x_flat", W_regularizer=reg()),
-        Activation('sigmoid'),
-        Reshape(input_shape, name="generator_x")],
+def model_generator(
+        latent_dim, input_shape, hidden_dim=1024, reg=lambda: l1(1e-5), batch_norm_mode=0):
+    return Sequential(
+        layers=[
+            Dense(hidden_dim // 4, name="generator_h1", input_dim=latent_dim, W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            Dense(hidden_dim // 2, name="generator_h2", W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            Dense(hidden_dim, name="generator_h3", W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            Dense(np.prod(input_shape), name="generator_x_flat", W_regularizer=reg()),
+            Activation('sigmoid'),
+            Reshape(input_shape, name="generator_x")],
         name="generator")
 
 
-def model_discriminator(input_shape, hidden_dim=1024, reg=lambda: l1l2(1e-5, 1e-5), dropout=0.5, batch_norm_mode=1,
-                        output_activation="sigmoid"):
-    return Sequential([
-        Flatten(name="discriminator_flatten", input_shape=input_shape),
-        Dense(hidden_dim, name="discriminator_h1", W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        dropout_layer(dropout),
-        Dense(hidden_dim / 2, name="discriminator_h2", W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        dropout_layer(dropout),
-        Dense(hidden_dim / 4, name="discriminator_h3", W_regularizer=reg()),
-        batch_norm(batch_norm_mode),
-        LeakyReLU(0.2),
-        dropout_layer(dropout),
-        Dense(1, name="discriminator_y", W_regularizer=reg()),
-        Activation(output_activation)],
+def model_discriminator(
+        input_shape, hidden_dim=1024, reg=lambda: l1l2(1e-5, 1e-5), dropout=0.5,
+        batch_norm_mode=1,
+        output_activation="sigmoid"):
+    return Sequential(
+        layers=[
+            Flatten(name="discriminator_flatten", input_shape=input_shape),
+            Dense(hidden_dim, name="discriminator_h1", W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            dropout_layer(dropout),
+            Dense(hidden_dim // 2, name="discriminator_h2", W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            dropout_layer(dropout),
+            Dense(hidden_dim // 4, name="discriminator_h3", W_regularizer=reg()),
+            batch_norm(batch_norm_mode),
+            LeakyReLU(0.2),
+            dropout_layer(dropout),
+            Dense(1, name="discriminator_y", W_regularizer=reg()),
+            Activation(output_activation)],
         name="discriminator")
 
 
-def example_gan(adversarial_optimizer, path, opt_g, opt_d, nb_epoch, generator, discriminator, latent_dim,
-                targets=gan_targets, loss='binary_crossentropy'):
+def example_gan(
+        adversarial_optimizer, path,
+        opt_g, opt_d,
+        nb_epoch,
+        generator, discriminator,
+        latent_dim,
+        targets=gan_targets, loss='binary_crossentropy'):
     csvpath = os.path.join(path, "history.csv")
     if os.path.exists(csvpath):
         print("Already exists: {}".format(csvpath))
@@ -88,7 +98,8 @@ def example_gan(adversarial_optimizer, path, opt_g, opt_d, nb_epoch, generator, 
 
     # build adversarial model
     model = AdversarialModel(base_model=gan,
-                             player_params=[generator.trainable_weights, discriminator.trainable_weights],
+                             player_params=[generator.trainable_weights,
+                                            discriminator.trainable_weights],
                              player_names=["generator", "discriminator"])
     model.adversarial_compile(adversarial_optimizer=adversarial_optimizer,
                               player_optimizers=[opt_g, opt_d],
@@ -109,8 +120,15 @@ def example_gan(adversarial_optimizer, path, opt_g, opt_d, nb_epoch, generator, 
     callbacks = [generator_cb]
     if K.backend() == "tensorflow":
         callbacks.append(
-            TensorBoard(log_dir=os.path.join(path, 'logs'), histogram_freq=0, write_graph=True, write_images=True))
-    history = model.fit(x=xtrain, y=y, validation_data=(xtest, ytest), callbacks=callbacks, nb_epoch=nb_epoch,
+            TensorBoard(
+                log_dir=os.path.join(path, 'logs'),
+                histogram_freq=0,
+                write_graph=True,
+                write_images=True))
+    history = model.fit(x=xtrain, y=y,
+                        validation_data=(xtest, ytest),
+                        callbacks=callbacks,
+                        nb_epoch=nb_epoch,
                         batch_size=32)
 
     # save history to CSV
